81e29b962d58b506f7dec873d54caae16436032b
AceLan Kao <acelan@acelan.idv.tw>
2014-02-24 19:28:05 +0800

    bot: add wait_order_succeed()
    Add wait_order_succeed() function to make sure we won't place order twice.
    
    Signed-off-by: AceLan Kao <acelan@acelan.idv.tw>

--- cryptsy_bot.php
+++ cryptsy_bot.php
@@ -14,7 +14,9 @@ abstract class cryptsy_bot
 
 		$this->cryptsy = new Cryptsy( $this->key, $this->secret);
 		// get data
-		$this->init($this->update_data());
+		$data = $this->update_data();
+		$this->init($data);
+		$this->order_count = sizeof($data["my_orders"]);
 		while(1)
 		{
 			sleep($this->tick_timeout);
@@ -27,9 +29,21 @@ abstract class cryptsy_bot
 	protected abstract function init($data);
 	protected abstract function tick($data);
 	protected function set_key($key,$secret,$label) {$this->key=$key; $this->secret=$secret; $this->label=$label;}
-	protected function create_buy_order($price, $quantity) {$this->cryptsy->create_buy_order($this->label,$price,$quantity);}
-	protected function create_sell_order($price, $quantity) {$this->cryptsy->create_sell_order($this->label,$price,$quantity);}
-	protected function cancel_market_orders() {$this->cryptsy->cancel_market_orders($this->label);}
+	protected function create_buy_order($price, $quantity)
+	{
+		$this->cryptsy->create_buy_order($this->label,$price,$quantity);
+		$this->order_count = $this->wait_order_succeed($this->order_count+1);
+	}
+	protected function create_sell_order($price, $quantity)
+	{
+		$this->cryptsy->create_sell_order($this->label,$price,$quantity);
+		$this->order_count = $this->wait_order_succeed($this->order_count+1);
+	}
+	protected function cancel_market_orders()
+	{
+		$this->cryptsy->cancel_market_orders($this->label);
+		$this->order_count = $this->wait_order_succeed($this->order_count+1);
+	}
 	/*
 	 * [cur_buy_price] => 0.00000209
 	 * [cur_sell_price] => 0.00000210
@@ -101,9 +115,30 @@ abstract class cryptsy_bot
 	protected $tick_timeout;
 
 // private section
+	private function wait_order_succeed($num_orders)
+	{
+		// won't leave the loop if we can't get correct number of orders
+		$counter = 0;
+		while( 1 )
+		{
+			$counter++;
+			$my_orders = $this->cryptsy->get_orders($this->label);
+			if( (sizeof($my_orders) == $num_orders))
+				break;
+
+			if($counter > 10)      // re-try 10 times
+			{
+				// leave anyway
+				break;
+			}
+			sleep(3);
+		}
+		return sizeof($my_orders);
+	}
 	private $cryptsy;
 	private $key;
 	private $secret;
 	private $label;
+	private $order_count;
 }
 ?>
