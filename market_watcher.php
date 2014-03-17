5c1bf39e836dbc0dfdcdb7e570c733a3c1252575
AceLan Kao <acelan@acelan.idv.tw>
2014-03-17 16:01:04 +0800

    market_watcher: to monitor the market
    market_watcher will do the pseudo trade of the market data,
    so it can tell you which coin is worth to do auto trading
    
    Signed-off-by: AceLan Kao <acelan@acelan.idv.tw>

---
+++ market_watcher.php
@@ -0,0 +1,46 @@
+<?php
+require_once "cryptsy_lib.php";
+require_once "config.php";
+
+$cryptsy = new Cryptsy( $config['public_key'] , $config['private_key']);
+$markets = array();
+
+while(1)
+{
+	$data = $cryptsy->get_marketdata();
+	$markets = array_merge((array)$data["markets"], $markets);
+	foreach( $markets as $market)
+	{
+		$count = 0;
+		$b_c = 0;
+		$s_c = 0;
+		$trades = array_reverse($market["recenttrades"]);
+		foreach( $trades as $trade)
+		{
+			if( $count == 0)
+			{
+				$buy_price = $trade["price"]*$config["profit_percent"];
+				$sell_price = $trade["price"]*$config["stop_lost_percent"];
+				$count++;
+			}
+
+			if( $trade["price"] >= $buy_price)
+			{
+				$b_c++;
+				$buy_price = $trade["price"];
+			}
+			else if( $trade["price"] <= $sell_price)
+			{
+				$s_c++;
+				$sell_price = $trade["price"];
+			}
+		}
+		// we like waves
+		if(($b_c != 0) && ($s_c != 0))
+			print($market["label"]." - buy_count = ".$b_c." , sell_count = ".$s_c."\n");
+	}
+	// get data once a minute
+	sleep(60);
+}
+
+?>
